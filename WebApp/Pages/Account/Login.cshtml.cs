6fa42c4f3dc9b8a16446f763ffd3b62584a92490
Nate Thompson <nthompson2@avidxchange.com>
2022-12-30 14:19:43 -0500

    Initial Push

---
+++ WebApp/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Account;
+
+public class Login : PageModel
+{
+    [BindProperty]
+    public Credential Credential { get; set; }
+    public void OnGet()
+    {
+        
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if(!ModelState.IsValid) return Page();
+
+        if (Credential.UserName == "admin" && Credential.Password == "password")
+        {
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Name, "admin"),
+                new(ClaimTypes.Email, "admin@mywebsite.com")
+            };
+
+            var identity = new ClaimsIdentity(claims, "MyCookie");
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync("MyCookie",claimsPrincipal);
+
+            return RedirectToPage("/Index");
+        }
+
+        return Page();
+    }
+}
+
+public class Credential
+{
+    [Required] 
+    public string UserName { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+}
